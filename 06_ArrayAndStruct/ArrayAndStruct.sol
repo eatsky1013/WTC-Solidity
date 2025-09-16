a7cad0fc62b6e22fcedfedbbf0e632840d31ab5d
eatsky1013 <eatsky1013@gmail.com>
2025-09-16 20:22:27 +0900

    update: 06_ArrayAndStruct

--- 06_ArrayAndStruct/ArrayAndStruct.sol
+++ 06_ArrayAndStruct/ArrayAndStruct.sol
@@ -1,55 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
-contract ArrayTypes {
 
+contract ArrayTypes {
     // 固定长度 Array
-    uint[8] array1;
-    bytes1[5] array2;
-    address[100] array3;
+    uint[8] public array1;
+    bytes1[5] public array2;
+    address[100] public array3;
 
     // 可变长度 Array
-    uint[] array4;
-    bytes1[] array5;
-    address[] array6;
-    bytes array7;
+    uint[] public array4;
+    bytes1[] public array5;
+    address[] public array6;
+    bytes public array7;
 
     // 初始化可变长度 Array
-    uint[] array8 = new uint[](5);
-    bytes array9 = new bytes(9);
-    //  给可变长度数组赋值
-    function initArray() external pure returns(uint[] memory){
+    uint[] public array8 = new uint[](5);
+    bytes public array9 = new bytes(9);
+    
+    // 给可变长度数组赋值
+    function initArray() external pure returns(uint[] memory) {
         uint[] memory x = new uint[](3);
         x[0] = 1;
         x[1] = 3;
         x[2] = 4;
-        return(x);
+        return x;
     }  
-    function arrayPush() public returns(uint[] memory){
-        uint[2] memory a = [uint(1),2];
+    
+    function arrayPush() public returns(uint[] memory) {
+        uint[2] memory a = [uint(1), 2];
         array4 = a;
         array4.push(3);
         return array4;
     }
 }
 
-pragma solidity ^0.8.21;
 contract StructTypes {
     // 结构体 Struct
-    struct Student{
+    struct Student {
         uint256 id;
         uint256 score; 
     }
-    Student student; // 初始一个student结构体
-    //  给结构体赋值
+    
+    Student public student; // 初始一个student结构体
+    
+    // 给结构体赋值
     // 方法1:在函数中创建一个storage的struct引用
-    function initStudent1() external{
+    function initStudent1() external {
         Student storage _student = student; // assign a copy of student
         _student.id = 11;
         _student.score = 100;
     }
 
     // 方法2:直接引用状态变量的struct
-    function initStudent2() external{
+    function initStudent2() external {
         student.id = 1;
         student.score = 80;
     }
@@ -65,15 +68,20 @@ contract StructTypes {
     }
 }
 
-pragma solidity ^0.8.21;
 contract EnumTypes {
     // 将uint 0， 1， 2表示为Buy, Hold, Sell
     enum ActionSet { Buy, Hold, Sell }
+    
     // 创建enum变量 action
-    ActionSet action = ActionSet.Buy;
+    ActionSet public action = ActionSet.Buy;
 
     // enum可以和uint显式的转换
-    function enumToUint() external view returns(uint){
+    function enumToUint() external view returns(uint) {
         return uint(action);
+    }
+    
+    // 设置enum值
+    function setAction(ActionSet _action) external {
+        action = _action;
     }
 }
